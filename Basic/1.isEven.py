e445587b6e73edd8d59b1eea5cbfa938e5e54107
abidkhokhar <dev.abidkk@gmail.com>
2025-05-14 22:30:39 +0500

    even odd

---
+++ Basic/1.isEven.py
@@ -0,0 +1,13 @@
+def isEven(x):
+    try:
+        if x%2 == 0:
+            return f"{x}: is Even"
+        else:
+            return f"{x} is Odd"
+    except Exception as error:
+        return f"Error: Please provide a valid Integer: {error}"
+    
+    
+print(isEven(100)) # Even
+print(isEven(101)) # Odd
+print(isEven("a12")) # Error
